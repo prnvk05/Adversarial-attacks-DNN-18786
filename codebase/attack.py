3d6722335345f8d3d5b53c7b00fbdb4aec0381e0
yashg2 <yashg2@andrew.cmu.edu>
2020-10-23 04:09:59 +0000

    fgsm implemented

--- codebase/attack.py
+++ codebase/attack.py
@@ -15,6 +15,8 @@ from collections import OrderedDict
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
+device = torch.device("cuda" if (torch.cuda.is_available()) else "cpu")
+#%%
 model = resnet32().cuda()
 checkpoint = torch.load('/root/adversarial/saved_model/resnet32-d509ac18.th')
 
@@ -33,7 +35,7 @@ val_loader = torch.utils.data.DataLoader(
         transforms.ToTensor(),
         normalize,
     ])),
-    batch_size=128, shuffle=False,
+    batch_size=1, shuffle=False,
     num_workers=4, pin_memory=True)
 
 def validation(model,valid_dataloader):
@@ -53,4 +55,97 @@ def validation(model,valid_dataloader):
 
 
 print(validation(model,val_loader))
+# %%
+
+# FGSM attack code
+
+def fgsm_attack(image, epsilon, data_grad):
+    # Collect the element-wise sign of the data gradient
+    sign_data_grad = data_grad.sign()
+    # Create the perturbed image by adjusting each pixel of the input image
+    perturbed_image = image + epsilon*sign_data_grad
+    # Adding clipping to maintain [0,1] range
+    perturbed_image = torch.clamp(perturbed_image, 0, 1)
+    # Return the perturbed image
+    return perturbed_image
+
+
+
+def test( model, device, test_loader, epsilon ):
+
+    # Accuracy counter
+    correct = 0
+    adv_examples = []
+
+    # Loop over all examples in test set
+    for data, target in test_loader:
+
+        # Send the data and label to the device
+        data, target = data.to(device), target.to(device)
+
+        # Set requires_grad attribute of tensor. Important for Attack
+        data.requires_grad = True
+
+        # Forward pass the data through the model
+        output = model(data)
+        init_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+
+        # import pd
+        # b; pdb.set_trace()
+        # If the initial prediction is wrong, dont bother attacking, just move on
+        # print(init_pred,target)
+        # init_pred = init_pred
+        # print(init_pred,target)
+        if init_pred.item() != target.item():
+            continue
+
+        # Calculate the loss
+        loss = F.nll_loss(output, target)
+
+        # Zero all existing gradients
+        model.zero_grad()
+
+        # Calculate gradients of model in backward pass
+        loss.backward()
+
+        # Collect datagrad
+        data_grad = data.grad.data
+
+        # Call FGSM Attack
+        perturbed_data = fgsm_attack(data, epsilon, data_grad)
+
+        # Re-classify the perturbed image
+        output = model(perturbed_data)
+
+        # Check for success
+        final_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+        if final_pred.item() == target.item():
+            correct += 1
+            # Special case for saving 0 epsilon examples
+            if (epsilon == 0) and (len(adv_examples) < 5):
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+        else:
+            # Save some adv examples for visualization later
+            if len(adv_examples) < 5:
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+
+    # Calculate final accuracy for this epsilon
+    final_acc = correct/float(len(test_loader))
+    print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader), final_acc))
+
+    # Return the accuracy and an adversarial example
+    return final_acc, adv_examples
+
+
+accuracies = []
+examples = []
+
+# Run test for each epsilon
+# for eps in epsilons:
+
+acc, ex = test(model, device, val_loader, 0.3)
+accuracies.append(acc)
+examples.append(ex)
 # %%
