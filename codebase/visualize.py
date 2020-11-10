63a04c29a7da48cf2ef7f64f9d0938b81c1c1c12
AND2797 <aditya.das2797@gmail.com>
2020-11-10 04:58:21 +0000

    vios

--- codebase/visualize.py
+++ codebase/visualize.py
@@ -16,7 +16,7 @@ import torchvision.transforms as transforms
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
-
+# %%
 with open('/root/Adversarial-attacks-DNN-18786/saved_model/adv_examples.pkl', 'rb') as f:
     mynewlist = pickle.load(f)
 with open('/root/Adversarial-attacks-DNN-18786/saved_model/adv_examples1.pkl', 'rb') as f:
@@ -67,7 +67,12 @@ load_accuracies = np.load('/root/Adversarial-attacks-DNN-18786/PGD_DATA/alpha_ac
 adv_examples = np.load('/root/Adversarial-attacks-DNN-18786/PGD_DATA/alpha_adv_examples_pgd.npy', allow_pickle = True)
 alpha_epsilon_pgd = np.load('/root/Adversarial-attacks-DNN-18786/PGD_DATA/alpha_epsilons_pgd.npy')
 
+
+
+
 # %%
+# 0,3,4 - car
+# 0,0,4 - sip
 inv_normalize = transforms.Normalize(
     mean=[-0.485/0.229, -0.456/0.224, -0.406/0.255],
     std=[1/0.229, 1/0.224, 1/0.255]
@@ -81,21 +86,15 @@ def imshow(img, acc):
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
     # plt.show()
-    plt.savefig(f'/root/Adversarial-attacks-DNN-18786/pics/ship_og_{acc}.png')
+    plt.savefig(f'/root/Adversarial-attacks-DNN-18786/pics/car_og_{acc}.png')
 
 
-# %%
-# 0,3,4 - car
-# 0,0,4 - sip
-
-
-
-# im_og = imshow(torchvision.utils.make_grid(torch.from_numpy(adv_examples[0][0][4])))
-im1 = adv_examples[0][0][3]
-im2= adv_examples[1][2][3]
-im3= adv_examples[2][2][3]
-im4= adv_examples[3][2][3]
-im5= adv_examples[4][2][3]
+# im_og = imshow(torchvision.utils.make_grid(torch.from_numpy(adv_examples[0][3][4])), 'loda')
+im1 = adv_examples[0][3][3]
+im2= adv_examples[1][6][3]
+im3= adv_examples[2][6][3]
+im4= adv_examples[3][6][3]
+im5= adv_examples[4][6][3]
 
 ims = [im1, im2, im3, im4, im5]
 
@@ -103,3 +102,5 @@ for idx, im in enumerate(ims):
     imshow(torchvision.utils.make_grid(torch.from_numpy(im)), str(load_accuracies[idx]))
 
 
+
+# %%
