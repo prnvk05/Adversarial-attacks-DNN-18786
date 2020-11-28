93a622d50c68453b7e9f1dc24508986374ebb607
AND2797 <aditya.da2797@gmail.com>
2020-11-28 04:39:38 +0000

    refactor

--- codebase/inpainting /inpt_test_file.py
+++ codebase/inpainting /inpt_test_file.py
@@ -65,38 +65,68 @@ class custom_loader():
         image = self.transform(image)
         return image, torch.as_tensor(label).long()
 
-# og_examples_format = np.load('/root/og_images10k.npz', allow_pickle = True)['arr_0']
-# inpaint_examples_format = np.load('/root/test_inpainted10k.npz', allow_pickle = True)['arr_0']
-# inpaint_labels = np.load('/root/label_inpainted10k.npz', allow_pickle = True)['arr_0']
-# adv_examples_cutout_format = np.load('/root/Adversarial-attacks-DNN-18786/codebase/adv_images_fgsm.npz', allow_pickle  = True)['arr_0']
-# adv_labels_cutout_format = np.load('/root/Adversarial-attacks-DNN-18786/codebase/adv_labels_fgsm.npz', allow_pickle = True)['arr_0']
+### ORIGINAL & INPAINTED WITHOUT NOISE
+og_examples_format = np.load('/root/og_new_spots_images10k.npz', allow_pickle = True)['arr_0']
+og_labels_format = np.load('/root/label_new_spots_inpainted10k.npz', allow_pickle = True)['arr_0']
 
-adv_examples_og = np.load('/root/og_adv_spots_images10k.npz', allow_pickle = True)['arr_0']
-adv_examples_inpainted = np.load('/root/test_adv_spots_inpainted10k.npz', allow_pickle = True)['arr_0']
-adv_examples_labels = np.load('/root/label_adv_spots_inpainted10k.npz', allow_pickle = True)['arr_0']
+inpaint_examples_format = np.load('/root/test_new_spots_inpainted10k.npz', allow_pickle = True)['arr_0']
+inpaint_labels = np.load('/root/label_new_spots_inpainted10k.npz', allow_pickle = True)['arr_0']
 
-# og_examples_format = og_
-# inpaint_examples_format = inpaint_examples
-# inpaint_labels_format = inpaint_labels
+### ORIGINAL & INPAINTED WITH ADVERSARIAL NOISE
+og_adv_examples_format = np.load('/root/og_newadv_spots_images10k.npz', allow_pickle=True)['arr_0']
+og_adv_labels_format = np.load('/root/label_newadv_spots_inpainted10k.npz', allow_pickle=True)['arr_0']
+
+adv_inpainted_examples_format = np.load('/root/test_newadv_spots_inpainted10k.npz', allow_pickle = True)['arr_0']
+adv_inpainted_labels_format = np.load('/root/ly10k.npz', allow_pickle = True)['arr_0']
+
+
 
 # %%
-# custom_dataset_inpt = custom_loader(inpaint_examples_format, inpaint_labels_format)
-# custom_dataset_og = custom_loader(og_examples_format, inpaint_labels_format)
-# custom_dataset_cutout = custom_loader(og_examples_format, inpaint_labels_format, cutout=True)
-custom_dataset_adv_cutout = custom_loader(adv_examples_og, adv_examples_labels, cutout = True, i = 18, j = 2)
-# custom_dataset_adv_inpainted = custom_loader(adv_examples_og, adv_examples_labels)
-# test_loader_inpt = torch.utils.data.DataLoader(custom_dataset_inpt,
+### WITHOUT ADV NOISE
+# custom_dataset_inpt = custom_loader(inpaint_examples_format, inpaint_labels)
+# custom_dataset_og_cutout = custom_loader(og_examples_format, og_labels_format, cutout = True, i = 18, j = 2)
+# custom_dataset_og = custom_loader(og_examples_format, og_labels_format)
+
+# test_loader_inpt_nonoise = torch.utils.data.DataLoader(custom_dataset_inpt,
+#     batch_size=128, shuffle=False,
+#     num_workers=4, pin_memory=True)
+# test_loader_og_nonoise = torch.utils.data.DataLoader(custom_dataset_og,
+#     batch_size=128, shuffle=False,
+#     num_workers=4, pin_memory=True)
+# test_loader_cutout_nonoise = torch.utils.data.DataLoader(custom_dataset_og_cutout,
 #     batch_size=128, shuffle=False,
 #     num_workers=4, pin_memory=True)
 
-# test_loader_og = torch.utils.data.DataLoader(custom_dataset_og,
+
+###  WITH ADV NOISE
+# custom_dataset_adv_inpt = custom_loader(adv_inpainted_examples_format, adv_inpainted_labels_format)
+# custom_dataset_adv_og = custom_loader(og_adv_examples_format, og_adv_labels_format)
+# custom_dataset_adv_cutout = custom_loader(og_adv_examples_format, adv_inpainted_labels_format, cutout = True, i = 18, j = 2)
+
+# test_loader_adv_cutout_noise = torch.utils.data.DataLoader(custom_dataset_adv_cutout,
 #     batch_size=128, shuffle=False,
 #     num_workers=4, pin_memory=True)
+# test_loader_adv_inpt_noise = torch.utils.data.DataLoader(custom_dataset_adv_inpt,
+#     batch_size=128, shuffle=False,
+#     num_workers=4, pin_memory=True)
+# test_loader_adv_og_noise = torch.utils.data.DataLoader(custom_dataset_adv_og,
+#     batch_size=128, shuffle=False,
+#     num_workers=4, pin_memory=True)
+
+
+
+
+# custom_dataset_adv_cutout = custom_loader(adv_examples_og, adv_examples_labels, cutout = True, i = 18, j = 2)
+# custom_dataset_adv_inpainted = custom_loader(adv_examples_og, adv_examples_labels)
+
+### LOADER FOR ORIGINAL INPAINTED W
+
 
 # test_loader_cutout = torch.utils.data.DataLoader(custom_dataset_cutout, batch_size = 128, 
 #                                                 shuffle = False, num_workers = 4, pin_memory = True)
 
-test_loader_adv_cutout = torch.utils.data.DataLoader(custom_dataset_adv_cutout, batch_size = 128, shuffle = False, num_workers = 4, pin_memory = True)
+
+# test_loader_adv_cutout = torch.utils.data.DataLoader(custom_dataset_adv_cutout, batch_size = 128, shuffle = False, num_workers = 4, pin_memory = True)
 
 # test_loader_adv_inpainted = torch.utils.data.DataLoader(custom_dataset_adv_inpainted, batch_size = 128, shuffle = False, 
 #                                                         num_workers = 4, pin_memory = True)
@@ -122,24 +152,39 @@ def validation(model,valid_dataloader):
   return top1_accuracy/total
 
 ### GRID SEARCH
-# accs = []
-# for i in range(1, 20):
-#     for j in range(1, 20):
-#         custom_dataset_adv_cutout = custom_loader(adv_examples_cutout_format, adv_labels_cutout_format, cutout = True, i = i, j = j)
+accs = []
+for i in range(30, 50):
+    # for j in range(1,):
+    custom_dataset_adv_cutout = custom_loader(og_adv_examples_format, og_adv_labels_format, cutout = True, i = i, j = 2)
+    test_loader_adv_cutout_noise = torch.utils.data.DataLoader(custom_dataset_adv_cutout,
+        batch_size=128, shuffle=False,
+        num_workers=4, pin_memory=True)
+    
+    result_cutout_noise = validation(model, test_loader_adv_cutout_noise)
+    print(i, 2, result_cutout_noise)
+        
 
-#         test_loader_adv_cutouts = torch.utils.data.DataLoader(custom_dataset_adv_cutout, batch_size = 128, 
-#                                                 shuffle = False, num_workers = 4, pin_memory = True)
+
+#### RESULT WITHOUT ADV NOISE
+# result_inpainted_nonoise = validation(model, test_loader_inpt_nonoise)
+# result_cutout_nonoise = validation(model, test_loader_cutout_nonoise)
+# result_og_nonoise = validation(model, test_loader_og_nonoise)
+
+#### RESULT WITH ADV NOISE 
+# result_inpainted_noise = validation(model, test_loader_adv_inpt_noise)
+# result_cutout_noise = validation(model, test_loader_adv_cutout_noise)
+# result_og_noise =  validation(model, test_loader_adv_og_noise)
 
 
-
-# print("inpainted:", validation(model, test_loader_inpt))
-# print("original:", validation(model, test_loader_og))
-# print("cutout:", validation(model, test_loader_cutout))
-result = validation(model, test_loader_adv_cutout)
-
-# print("cutout adv:", i,j,result)
-# accs.append((i,j,result))
-print(result)
+# print("#### NO NOISE ####")
+# print("Inpainted:", result_inpainted_nonoise)
+# print("Cutout:",result_cutout_nonoise)
+# print("Original:",result_og_nonoise)
+# print()
+# print("#### ADV NOISE ####")
+# print("Inpainted:", result_inpainted_noise)
+# print("Cutout:", result_cutout_noise)
+# print("Original:", result_og_noise)
 
 # %% plot acc
 from scipy.interpolate import make_interp_spline, BSpline
